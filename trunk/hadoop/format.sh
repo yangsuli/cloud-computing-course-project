6d04fb68e719773c482a02b109f6ccedcbb42bce
yangsuli@gmail.com <yangsuli@gmail.com@bab081c9-e616-69dc-3827-801a018ce7ff>
2012-12-06 20:21:13 +0000

    Now both RPC and Xceiver logging seems to be working

--- trunk/hadoop/format.sh
+++ trunk/hadoop/format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -fr /home/openflow/hadoop-tmp/*
-rsync -avz --delete /home/openflow/hadoop-tmp/ openflow@192.168.1.128:/home/openflow/hadoop-tmp
+rsync -avz --delete /home/openflow/hadoop-tmp/ openflow@192.168.8.134:/home/openflow/hadoop-tmp
 /home/openflow/hadoop-1.0.4/bin/hadoop namenode -format
--- trunk/hadoop/svnsync.sh
+++ trunk/hadoop/svnsync.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 rsync -avz *.sh suli@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
-rsync -avz --include="/src" --include="/conf" hadoop-1.0.4 suli@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
+rsync -avz hadoop-1.0.4/src suli@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
+rsync -avz hadoop-1.0.4/conf suli@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
+ssh suli@192.168.8.1 "cd /scratch/suli/838/cloud-computing-course-project/; svn ci"
+rsync -avz *.sh suli@192.168.8.134:/home/openflow/hadoop-1.0.4/
+rsync -avz hadoop-1.0.4/src suli@192.168.8.134:/home/openflow/hadoop-1.0.4/
+rsync -avz hadoop-1.0.4/conf suli@192.168.8.134:/home/openflow/hadoop-1.0.4/
+ssh openflow@192.168.8.134 "cd hadoop-1.0.4; ant"
 
