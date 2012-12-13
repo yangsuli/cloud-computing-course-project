b22525b2e3dfe8ca2ef82e839f22936829736c0e
yangsuli@gmail.com <yangsuli@gmail.com@bab081c9-e616-69dc-3827-801a018ce7ff>
2012-12-13 22:53:56 +0000

    add -fPIC flag to ensure compiling on the cluster

--- trunk/hadoop/src/core/org/apache/hadoop/util/compile.sh
+++ trunk/hadoop/src/core/org/apache/hadoop/util/compile.sh
@@ -6,5 +6,5 @@ javah -jni org.apache.hadoop.util.ADGSetSocketTOS
 cd $pwd
 mv ../../../../org_apache_hadoop_util_ADGSetSocketTOS.h org_apache_hadoop_util_ADGSetSocketTOS.h
 
-gcc -shared -I/usr/lib/jvm/java-6-sun-1.6.0.26/include -I/usr/lib/jvm/java-6-sun-1.6.0.26/include/linux SetSocketTOS.c -o libsetsocktos.so 
+gcc -shared -fPIC -I/usr/lib/jvm/java-6-sun-1.6.0.26/include -I/usr/lib/jvm/java-6-sun-1.6.0.26/include/linux SetSocketTOS.c -o libsetsocktos.so 
 cp libsetsocktos.so /home/openflow/hadoop-1.0.4/libexec/../lib/native/Linux-i386-32/
