c4a135981d83a5da1ba51140e7c2f6b86f889180
yangsuli@gmail.com <yangsuli@gmail.com@bab081c9-e616-69dc-3827-801a018ce7ff>
2012-12-13 22:28:11 +0000

    Now the java jni library could be loaded However, we are unable to get the FileDescripter jobject from ScoketImpl I will check whether this is due to my VM or the whole cluster

--- trunk/svnsync.sh
+++ trunk/svnsync.sh
@@ -8,4 +8,4 @@ rsync -avz openflow@192.168.8.133:/home/openflow/hadoop-1.0.4/bin /scratch/suli/
 #openflow@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
 #rsync -avz --include="/src" --include="/conf" hadoop-1.0.4 suli@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
 
-ssh -t openflow@192.168.8.133 "rsync -avz --exclude=conf/hadoop-env.sh /home/openflow/hadoop-1.0.4/ openflow@192.168.8.134:/home/openflow/hadoop-1.0.4"
+ssh -t openflow@192.168.8.133 "rsync -avz --exclude=conf/hadoop-env.sh /home/openflow/hadoop-1.0.4/ openflow@192.168.8.134:/home/openflow/hadoop-1.0.4; cd /home/openflow/hadoop-1.0.4/src/core/org/apache/hadoop/util/; bash compile.sh"
