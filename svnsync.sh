30c23bbdcc211dfdd8f3b1a29cdd42daba3d8a39
yangsuli <yangsuli@gmail.com>
2012-12-14 09:02:50 +0000

    add packet parse directory

--- svnsync.sh
+++ svnsync.sh
@@ -4,8 +4,10 @@ rsync -avz openflow@192.168.8.133:/home/openflow/*.sh /scratch/suli/838/cloud-co
 rsync -avz openflow@192.168.8.133:/home/openflow/hadoop-1.0.4/src /scratch/suli/838/cloud-computing-course-project/hadoop
 rsync -avz openflow@192.168.8.133:/home/openflow/hadoop-1.0.4/conf /scratch/suli/838/cloud-computing-course-project/hadoop
 rsync -avz openflow@192.168.8.133:/home/openflow/hadoop-1.0.4/bin /scratch/suli/838/cloud-computing-course-project/hadoop
+rsync -avz openflow@192.168.8.133:/home/openflow/parse /scratch/suli/838/cloud-computing-course-project
 
 #openflow@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
 #rsync -avz --include="/src" --include="/conf" hadoop-1.0.4 suli@192.168.8.1:/scratch/suli/838/cloud-computing-course-project/hadoop
 
 ssh -t openflow@192.168.8.133 "rsync -avz --exclude=conf/hadoop-env.sh /home/openflow/hadoop-1.0.4/ openflow@192.168.8.134:/home/openflow/hadoop-1.0.4; cd /home/openflow/hadoop-1.0.4/src/core/org/apache/hadoop/util/; bash compile.sh"
+ssh -t openflow@192.168.8.133 "rsync -avz /home/openflow/parse openflow@192.168.8.134:/home/openflow;"
