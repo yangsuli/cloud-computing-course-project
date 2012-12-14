d9728c3f8861c87b4b5abe916941226448115828
yangsuli <yangsuli@gmail.com>
2012-12-14 07:13:43 +0000

    actually set the tos value

--- hadoop/src/core/org/apache/hadoop/util/SetSocketTOS.c
+++ hadoop/src/core/org/apache/hadoop/util/SetSocketTOS.c
@@ -79,27 +79,29 @@ JNIEXPORT jint Java_org_apache_hadoop_util_ADGSetSocketTOS_setSocketTOS(JNIEnv *
     //   int tos_local = flow_type;
     int tos_local;
     switch (flow_type) {
-        case 2: tos_local = 0x34;
+        case 1: tos_local = 0x01; //000001
                 break;
-        case 3: tos_local = 0x34;
+        case 2: tos_local = 0x02; //000010
                 break;
-        case 4: tos_local = 0x34;
+        case 3: tos_local = 0x03; //000011
                 break;
-        case 5: tos_local = 0x34;
+        case 4: tos_local = 0x04; //000100
                 break;
-        case 6: tos_local = 0x34;
+        case 5: tos_local = 0x05; //000101
                 break;
-        case 7: tos_local = 0x34;
+        case 6: tos_local = 0x06; //000110
                 break;
-        case 8: tos_local = 0x34;
+        case 7: tos_local = 0x07; //000111
                 break;
-        case 9: tos_local = 0x34;
+        case 8: tos_local = 0x08; //001000
                 break;
-        case 10: tos_local = 0x34;
+        case 9: tos_local = 0x09; //001001
                 break;
-        case 11: tos_local = 0x34;
+        case 10: tos_local = 0x0a; //001010
                 break;
-        case 12: tos_local = 0x34;
+        case 11: tos_local = 0x0b; //001011
+                break;
+        case 12: tos_local = 0x0c; //001100
                 break;
         default:
                 printf("unkown flow type %d\n", flow_type);
